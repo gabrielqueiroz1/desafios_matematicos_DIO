2e14ccf29509f9a9e1548273e8c00a2414b38c55
gabrielgqueiroz1 <gd37420@hotmail.com>
2021-09-30 19:50:50 -0300

    first commit

---
+++ mudanca.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        m = int(input())
+        if 0 <= m <= 45 or m == 360:
+            print("Bom Dia!!")
+        elif 90 <= m < 180:
+            print("Boa Tarde!!")
+        elif 180 <= m < 270:
+            print("Boa Noite!!")
+        elif 270 <= m < 360:
+            print("De Madrugada!!")
+    except EOFError:
+        break
+
+
+
